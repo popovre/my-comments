95418896d870a71988f65e44e7b60cbe5dfb8c0d
popovre <popov.roman.e@gmail.com>
2023-03-22 16:26:04 +0500

    добавляет конфиг webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: /View$/
+        }
+      })
+    ]
+  }
+};
